417900cbce2f5fe193b919fe3d4504251fb0df73
Sammy <samalaniz25@gmail.com>
2022-11-18 09:50:28 -0600

    fixed tree

--- MST-SCIPY/mst-scipy.py
+++ MST-SCIPY/mst-scipy.py
@@ -1,12 +1,13 @@
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import minimum_spanning_tree
 
-X = csr_matrix([[0, 1.1, 1.7, 0, 3.8, 0],
+X = csr_matrix([[0, 1.1, 1.7, 0, 2.6, 0],
                 [0, 0, 0, 3.1, 0, 0],
                 [0, 0, 0, 0, 3.8, 0],
                 [0, 0, 0, 0, 0, 3.7],
                 [0, 0, 0, 0, 0, 2.1],
                 [0, 0, 0, 0, 0, 0]])
+
 
 Tcsr = minimum_spanning_tree(X)
 
